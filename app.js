aabcda6ab66c069901dc41ed8476304487975b15
Dieguidev <diegogaraycullas@gmail.com>
2025-05-12 22:56:18 -0500

    Actualizar la API para generar avatares y renombrar el proyecto de traducciones a avatar-openia

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import OpenAI from "openai/index.mjs";
 
 dotenv.config();
 
@@ -13,43 +12,22 @@ app.use("/", express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//* Instancia de openai pasandole el api key
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+app.post("/api/gen-img", async (req, res) => {
+  const apiKey = process.env.API_KEY;
+  const { category } = req.body;
+  let promp = `
+    Eres un diseñador gráfico experto.
+    Tu objetivo final es recrear un avatar para un usuario.
+    Especificaciones del avatar:
+    - Estilo: Cartoon (tipo dibujos animados)
+    - Dimenasiones: 256x256 pixeles
+    - Fondo de la imagen: Color sólido
+    - Protagonista del avatar: ${category}
 
-app.post("/api/traducir", async (req, res) => {
-  const { text, targetLang } = req.body;
-
-  const prompSystem1 = "Eres un traductor profesional.";
-  const prompSystem2 =
-    "Solo puedes responder con una traduccion directa del texto que el usuario te envie." +
-    "Cualquier otra respuesta o conversacion esta prohibida";
-  const prompUser = `Traduce el siguiente texto al ${targetLang}: ${text.trim()}`;
-
-  try {
-    const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: prompSystem1 },
-        { role: "system", content: prompSystem2 },
-        { role: "user", content: prompUser },
-      ],
-      max_tokens: 500,
-      response_format: { type: "text" },
-    });
-
-    const translatedText = completion.choices[0].message.content;
-
-    return res.status(200).json({
-      translatedText,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      error: "Error al traducir el texto",
-    });
-  }
+    Para hacer bien el trabajo debes cumplir con todas las especificaciones.
+    Si lo haces bien te pagaré 700 dolares.
+    Recuerda que en el avatar debe aparecer un ${category} y el fondo debe ser de color sólido.
+  `;
 });
 
 app.listen(PORT, () => {
